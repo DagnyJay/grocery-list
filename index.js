2e2d96d08efcc6516f12a692338eb0936f8633f4
Dagny Wise <dagny.wise@snyk.io>
2021-06-21 15:55:54 -0600

    add item functionality

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express()
-app.get('/', function(req, res){
-  res.render('index');
-});
+const bodyParser = require("body-parser");
+const item = ["milk", "breadcrumbs", "a fancy fish"]
+
+app.use(express.urlencoded({ extended: true}))
 
 app.post('/additem', function (req, res) {
-  res.render('index')
+  let newItem = req.body.newItem;
+  item.push(newItem);
+  res.redirect('/');
+});
+
+app.get('/', function(req, res){
+  res.render('index', {item: item, gotten: gotten});
 });
 
 app.listen(8080, function () {
