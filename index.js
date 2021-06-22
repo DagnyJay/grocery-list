7d328b8e205c6eeaeaec8313fd5f82a780e4f036
Dagny Wise <dagny.wise@snyk.io>
2021-06-22 09:57:54 -0600

    remove item functionality

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const app = express()
 const bodyParser = require("body-parser");
-const item = ["milk", "breadcrumbs", "a fancy fish"]
+let groceryItems = ["milk", "breadcrumbs", "a fancy fish"]
 
 app.use(express.urlencoded({ extended: true}))
 
 app.post('/additem', function (req, res) {
   let newItem = req.body.newItem;
-  item.push(newItem);
+  groceryItems.push(newItem);
   res.redirect('/');
 });
 
 app.get('/', function(req, res){
-  res.render('index', {item: item, gotten: gotten});
+  res.render('index', {groceryItems: groceryItems});
 });
 
 app.listen(8080, function () {
   console.log('Running on port 8080!')
+});
+
+let gottenItems = 
+app.post("/removeItem", function(req, res) {
+  let gotItem = req.body.check;
+if (typeof gotItem === "string") {
+  gottenItems.push(gotItem);
+groceryItems.splice(groceryItems.indexOf(gotItem), 1);
+} else if (typeof gotItem === "object") {
+  for (let i = 0; i < gotItem.length; i++) { 
+    gottenItems.push(gotItem[i]);
+    groceryItems.splice(groceryItems.indexOf(gotItem[i]), 1);
+  }
+}
 });
 
 app.set('view engine', 'ejs');
