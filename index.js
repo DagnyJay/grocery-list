2dc4a53a043b8906d7bd114406b75c6fce1705f0
Dagny Wise <dagny.wise@snyk.io>
2021-06-24 08:48:22 -0600

    remove nonfunctional delete item code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require("body-parser");
 let groceryItems = ["milk", "breadcrumbs", "a fancy fish"]
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true}))
 
 app.post('/additem', function (req, res) {
@@ -19,18 +21,18 @@ app.listen(8080, function () {
   console.log('Running on port 8080!')
 });
 
-let gottenItems = 
-app.post("/removeItem", function(req, res) {
-  let gotItem = req.body.check;
-if (typeof gotItem === "string") {
-  gottenItems.push(gotItem);
-groceryItems.splice(groceryItems.indexOf(gotItem), 1);
-} else if (typeof gotItem === "object") {
-  for (let i = 0; i < gotItem.length; i++) { 
-    gottenItems.push(gotItem[i]);
-    groceryItems.splice(groceryItems.indexOf(gotItem[i]), 1);
-  }
-}
-});
+// let gottenItems = 
+// app.post("/removeItem", function(req, res) {
+//   let gotItem = req.body.check;
+// if (typeof gotItem === "string") {
+//   gottenItems.push(gotItem);
+// groceryItems.splice(groceryItems.indexOf(gotItem), 1);
+// } else if (typeof gotItem === "object") {
+//   for (let i = 0; i < gotItem.length; i++) { 
+//     gottenItems.push(gotItem[i]);
+//     groceryItems.splice(groceryItems.indexOf(gotItem[i]), 1);
+//   }
+// }
+// });
 
 app.set('view engine', 'ejs');
